9f49b8cb573ea4718f792bb4460b9d746b8d3e68
hamza-181 <hamzayh159@gmail.com>
2022-04-19 23:43:22 +0100

    comm

--- config.php
+++ config.php
@@ -1,23 +1,7 @@
 
 <?php
-/*
+
     //Location of your site which contains route.php
     $site_url = 'http://localhost/route/';
 
-
-    <?php
-    */
-// Informations d'identification
-
-$servername = "localhost";
-$username = "root";
-$password = "";
-$bd_name = "gtek";
-// Connexion à la base de données MySQL 
-$conn = mysqli_connect($servername, $username, $password, $bd_name);
- 
-// Vérifier la connexion
-if($conn === false){
-    die("ERREUR : Impossible de se connecter. " . mysqli_connect_error());
-}
 ?>
